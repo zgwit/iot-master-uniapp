1b050da2a5d8cd6eb2d431f02caa20c5ceb99879
Aero <aerogw@zgwit.com>
2022-09-26 02:41:56 +0800

    小优化

--- pages/hub/hub.vue
+++ pages/hub/hub.vue
@@ -1,5 +1,17 @@
 <template>
-	<view>
+	<view class="p20">
+		<uni-segmented-control :current="current" :values="tabs" @clickItem=""></uni-segmented-control>
+		<view class="content">
+			<view v-show="current === 0">
+				选项卡1的内容
+			</view>
+			<view v-show="current === 1">
+				选项卡2的内容
+			</view>
+			<view v-show="current === 2">
+				选项卡3的内容
+			</view>
+		</view>
 		在线资源库：产品，插件，模板，
 	</view>
 </template>
@@ -8,7 +20,8 @@
 	export default {
 		data() {
 			return {
-				
+				current: 0,
+				tabs: ["产品库","组件库","插件库"]
 			};
 		}
 	}
