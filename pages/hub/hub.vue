a7d455353ea5dbc1eb6fdd50a871b4d08d9f11b1
Aero <aerogw@zgwit.com>
2022-09-29 11:34:17 +0800

    内容优化

--- pages/hub/hub.vue
+++ pages/hub/hub.vue
@@ -1,17 +1,6 @@
 <template>
 	<view class="p20">
-		<uni-segmented-control :current="current" :values="tabs" @clickItem=""></uni-segmented-control>
-		<view class="content">
-			<view v-show="current === 0">
-				选项卡1的内容
-			</view>
-			<view v-show="current === 1">
-				选项卡2的内容
-			</view>
-			<view v-show="current === 2">
-				选项卡3的内容
-			</view>
-		</view>
+		<uni-search-bar @confirm="" @input="" placeholder="关键字" />
 		在线资源库：产品，插件，模板，
 	</view>
 </template>
